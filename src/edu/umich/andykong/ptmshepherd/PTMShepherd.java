38fb40d6f002485c6294c377ad054fbdaede86eb
Dmitry Avtonomov <dmt.avt@gmail.com>
2019-10-21 11:41:25 -0700

    Bump to 0.2.4

--- src/edu/umich/andykong/ptmshepherd/PTMShepherd.java
+++ src/edu/umich/andykong/ptmshepherd/PTMShepherd.java
@@ -14,7 +14,7 @@ import edu.umich.andykong.ptmshepherd.specsimilarity.*;
 public class PTMShepherd {
 
 	public static final String name = "PTM-Shepherd";
- 	public static final String version = "0.2.3";
+ 	public static final String version = "0.2.4";
 
 	static HashMap<String,String> params;
 	static TreeMap<String,ArrayList<String []>> datasets;
