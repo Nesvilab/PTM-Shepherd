2cfeccd540ca69933e49c0a2d7e6f0ece6f33c3d
Daniel Geiszler <danny.geisz@gmail.com>
2019-10-29 10:05:56 -0400

    v0.2.7 added privileged mods to annotation added intermediate file headers changed RT statistics

--- src/edu/umich/andykong/ptmshepherd/PTMShepherd.java
+++ src/edu/umich/andykong/ptmshepherd/PTMShepherd.java
@@ -14,7 +14,7 @@ import edu.umich.andykong.ptmshepherd.specsimilarity.*;
 public class PTMShepherd {
 
 	public static final String name = "PTM-Shepherd";
- 	public static final String version = "0.2.6";
+ 	public static final String version = "0.2.7";
 
 	static HashMap<String,String> params;
 	static TreeMap<String,ArrayList<String []>> datasets;
--- src/edu/umich/andykong/ptmshepherd/peakpicker/PeakAnnotator.java
+++ src/edu/umich/andykong/ptmshepherd/peakpicker/PeakAnnotator.java
@@ -12,26 +12,26 @@ public class PeakAnnotator {
 	ArrayList<Double> mod_diffs;
 	ArrayList<Integer> allowed_list;
 	String userMods = "";
-	ArrayList<String> vModNames;
+	ArrayList<String> vModNames; //privileged mods that can be matched with any mass in unimod
 	ArrayList<Double> vModMasses;
 
 	static final double C13delta = 1.003355;
 	static final double modEqual_tol = 0.001;
 	static final double mod_tol = 0.005;
 	//static final double mod_tol = Double.parseDouble(PTMShepherd.getParam(("precursor_tol")));
-	
+
 	int [] indices;
 	static final int maxDepth = 2;
 	static int cmaxDepth;
 	static boolean found, debug;
-	
+
 	public void annotateTSV(File inTSV, File outTSV) throws Exception {
         //ArrayList<String> vModNames = new ArrayList<>();
         //ArrayList<Double> vModMasses = new ArrayList<>();
 
 		//if(mods == null)
 		//	init(varMods);
-		
+
 		ArrayList<String> inFile = new ArrayList<>();
 		String cline;
 		BufferedReader in = new BufferedReader(new FileReader(inTSV));
@@ -45,7 +45,7 @@ public class PeakAnnotator {
 			masses[i-1] = Double.parseDouble(sp[0]);
 		}
 		String [][] annotations = annotate(masses);
-		
+
 		PrintWriter out = new PrintWriter(new FileWriter(outTSV));
 		out.print(inFile.get(0));
 		for(int i = 1; i <= maxDepth; i++)
@@ -59,7 +59,7 @@ public class PeakAnnotator {
 		}
 		out.close();
 	}
-	
+
 	public String [][] annotate(double [] masses) throws Exception {
 		String [][] res = new String[masses.length][maxDepth];
 		//init(userMods);
@@ -74,7 +74,7 @@ public class PeakAnnotator {
 		}
 		return res;
 	}
-	
+
 	public void dfs(int depth, double rem) {
 		if(Math.abs(rem) < mod_tol) {
 			found = true;
@@ -82,7 +82,7 @@ public class PeakAnnotator {
 		}
 //		if(debug) {
 //			System.out.print("depth: " + depth + " rem: " + rem);
-//			for(int i = 0; i < depth; i++) 
+//			for(int i = 0; i < depth; i++)
 //				System.out.printf(" [%.4f]",mod_diffs.get(indices[i]));
 //			System.out.println();
 //		}
@@ -96,7 +96,7 @@ public class PeakAnnotator {
 			indices[depth] = -1;
 		}
 	}
-	
+
 	public int [] checkMod(double v) {
 //		System.out.println(v);
 //		debug=true;
@@ -114,7 +114,23 @@ public class PeakAnnotator {
 				res[0] = i;
 				return res;
 			}
-		if(debug) 
+		for(int i = 0; i < vModNames.size(); i++){
+			double vPriv = v - vModMasses.get(i);
+			for(int j = 0; j < mods.size(); j++)
+				if(Math.abs(vPriv-mod_diffs.get(j)) < mod_tol) {
+					for(int k = 0; k < allowed_list.size(); k++)
+						if(allowed_list.get(k) == j) {
+							res[0] = i;
+							res[1] = j;
+							return res;
+						}
+					allowed_list.add(j);
+					res[0] = i;
+					res[1] = j;
+					return res;
+				}
+		}
+		if(debug)
 			for(int i = 0; i < allowed_list.size(); i++) {
 				System.out.printf("%s - %.4f\n",mods.get(allowed_list.get(i)),mod_diffs.get(allowed_list.get(i)));
 			}
@@ -124,7 +140,7 @@ public class PeakAnnotator {
 			dfs(0,v);
 			if(found) {
 				for(int j = 0; j < i; j++)
-					res[j] = indices[j]; 
+					res[j] = indices[j];
 				break;
 			}
 		}
@@ -137,7 +153,7 @@ public class PeakAnnotator {
 		debug = false;
 		return res;
 	}
-	
+
 	public void addMod(String tag, double v) {
 		for(int i = 0; i < mod_diffs.size(); i++) {
 			if(Math.abs(v-mod_diffs.get(i)) < modEqual_tol) {
@@ -148,7 +164,7 @@ public class PeakAnnotator {
 		mods.add(tag);
 		mod_diffs.add(v);
 	}
-	
+
 	public void init(String varMods) throws Exception {
 		mods = new ArrayList<String>(); //all modification names
 		mod_diffs = new ArrayList<Double>(); //all modification mass shifts
@@ -160,7 +176,7 @@ public class PeakAnnotator {
 
 		userMods = varMods;
 
-		if(!varMods.equals("")) {
+		if(!varMods.equals("None:0")) {
 			List<String> tMods = Arrays.asList(varMods.split(","));
 			for(int i = 0; i < tMods.size(); i++){
 				List<String> md = Arrays.asList(tMods.get(i).split(":"));
@@ -170,6 +186,9 @@ public class PeakAnnotator {
 				vModMasses.add(dMass);
 			}
 		}
+		for (int i = 0; i < vModNames.size(); i++) {
+			addMod(vModNames.get(i), vModMasses.get(i));
+		}
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("unimod_20191002.txt")));
 		String cline;
@@ -177,21 +196,18 @@ public class PeakAnnotator {
 		addMod("First isotopic peak",C13delta);
 		addMod("Second isotopic peak",2*C13delta);
 		addMod("Third isotopic peak",3*C13delta);
-		addMod("Oxidation or Hydroxylation",15.994915);
+		//addMod("Oxidation or Hydroxylation",15.994915);
 		//make isotopic peaks statically accessible
-		for(int i = 0; i < 3; i++){
+		//add user-defined mods to modification list
+        for (int i = 0; i < mods.size(); i++) {
 			allowed_list.add(i);
 		}
-		//add user-defined mods to modification list
-        for (int i = 0; i < vModNames.size(); i++) {
-            addMod(vModNames.get(i), vModMasses.get(i));
-            allowed_list.add(i);
-        };
 
 		while((cline = in.readLine())!= null) {
 			String [] sp = cline.split("\\t");
 			addMod(sp[0],Double.parseDouble(sp[1]));
 		}
+
 		in.close();
 
 		for(int i = 0; i < AAMasses.monoisotopic_masses.length; i++) {
