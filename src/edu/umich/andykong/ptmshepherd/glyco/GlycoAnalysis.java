7e956e148d65dadde97a2ec210dbf790eac9c73e
dpolasky <dpolasky@umich.edu>
2021-09-07 15:43:37 -0400

    fix too few decoys to perform FDR condition and update warning message

--- src/edu/umich/andykong/ptmshepherd/glyco/GlycoAnalysis.java
+++ src/edu/umich/andykong/ptmshepherd/glyco/GlycoAnalysis.java
@@ -231,8 +231,8 @@ public class GlycoAnalysis {
         double targetDecoyRatio = decoys / (double) targets;
         if (targetDecoyRatio < desiredRatio) {
             // not enough decoys to compute FDR - already above desired ratio. Do not update table
-            PTMShepherd.print(String.format("\tNot enough decoys to compute FDR at %.1f pct, initial pct %.2f", desiredRatio * 100, targetDecoyRatio * 100));
-            if (desiredRatio < targetDecoyRatio * 0.1) {
+            PTMShepherd.print(String.format("\tNot enough decoys to compute FDR at %.1f%%, started at %.2f%%", desiredRatio * 100, targetDecoyRatio * 100));
+            if (desiredRatio > targetDecoyRatio * 0.1) {
                 PTMShepherd.print(("\tNot enough decoys to compute FDR at 0.1 * initial ratio. Check data and parameters. No FDR calculation performed!\n"));
                 return;
             } else {
