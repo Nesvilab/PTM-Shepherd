d241fa6f01c2e6863a21baed55d6a8c828b5b4de
dpolasky <dpolasky@umich.edu>
2021-09-20 14:57:30 -0400

    bump to 1.2.4

--- src/edu/umich/andykong/ptmshepherd/PTMShepherd.java
+++ src/edu/umich/andykong/ptmshepherd/PTMShepherd.java
@@ -26,7 +26,7 @@ import static java.util.concurrent.Executors.newFixedThreadPool;
 public class PTMShepherd {
 
 	public static final String name = "PTM-Shepherd";
- 	public static final String version = "1.2.3";
+ 	public static final String version = "1.2.4";
 
 	static HashMap<String,String> params;
 	static TreeMap<String,ArrayList<String []>> datasets;
