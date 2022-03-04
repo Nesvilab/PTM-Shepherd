2af16b3ef5313dbf4e4c4e4214a0acb7c33531fc
danielgeiszler <danny.geisz@gmail.com>
2022-03-04 11:29:32 -0800

    Update to RC6

--- src/edu/umich/andykong/ptmshepherd/PTMShepherd.java
+++ src/edu/umich/andykong/ptmshepherd/PTMShepherd.java
@@ -27,7 +27,7 @@ import static java.util.concurrent.Executors.newFixedThreadPool;
 public class PTMShepherd {
 
 	public static final String name = "PTM-Shepherd";
- 	public static final String version = "2.0.0-RC5";
+ 	public static final String version = "2.0.0-RC6";
 
 	static HashMap<String,String> params;
 	static TreeMap<String,ArrayList<String []>> datasets;
