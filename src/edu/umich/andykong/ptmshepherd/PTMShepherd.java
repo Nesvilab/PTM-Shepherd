3304a6f608ad05cdf85e86afd4365460270d1bae
Dan Polasky <dpolasky@umich.edu>
2025-11-06 13:23:11 -0500

    bump to 3.0.10

--- src/edu/umich/andykong/ptmshepherd/PTMShepherd.java
+++ src/edu/umich/andykong/ptmshepherd/PTMShepherd.java
@@ -75,7 +75,7 @@ import umich.ms.fileio.filetypes.mzbin.MZBINFile.MZBINSpectrum;
 public class PTMShepherd {
 
 	public static final String name = "PTM-Shepherd";
- 	public static final String version = "3.0.9";
+ 	public static final String version = "3.0.10";
 
 	public static HashMap<String,String> params;
     public static TreeMap<String,ArrayList<String []>> datasets;
