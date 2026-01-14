585bdc78bf4ac418f992d22d43fa28627a35e52e
Dan Polasky <dpolasky@umich.edu>
2026-01-14 10:55:37 -0500

    fix normed mass score

--- src/edu/umich/andykong/ptmshepherd/glyco/GlycoAnalysis.java
+++ src/edu/umich/andykong/ptmshepherd/glyco/GlycoAnalysis.java
@@ -1469,10 +1469,9 @@ public class GlycoAnalysis {
         int isotopeErr = Math.toIntExact(Math.round(result.deltaMass - candidate.mass));
         double massError = result.deltaMass - candidate.mass - (isotopeErr * AAMasses.averagineIsotopeMass);
         candidate.massError = massError;
-        double massErrorPPM = (massError / (result.pepMass + candidate.mass)) * 1e6;
+        double massErrorPPM = Math.abs((massError / (result.pepMass + candidate.mass))) * 1e6;
         double massScore = (params.glycoPPMtol - massErrorPPM) / params.glycoPPMtol;
-        candidate.massErrorScore = Math.max(massScore, 0);
-        return candidate.massErrorScore;
+        return Math.max(massScore, 0);
     }
 
     /**
